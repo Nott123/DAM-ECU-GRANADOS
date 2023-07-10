72e0ae7f834a329b836927cbd64820636c186d0c
Adrian <lgk9hd.10gmail.com>
2023-07-10 15:10:35 -0500

    Segundo paso - Granados

---
+++ DAM_UC4_GRANADOS/app/src/main/java/com/example/dam_uc4_granados/MyNotaRecyclerViewAdapter.java
@@ -0,0 +1,60 @@
+package com.example.dam_uc4_granados;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.dam_uc4_granados.databinding.FragmentItemBinding;
+
+import java.util.List;
+
+
+public class MyNotaRecyclerViewAdapter extends RecyclerView.Adapter<MyNotaRecyclerViewAdapter.ViewHolder> {
+
+    private final List<Nota> mValues;
+    private final NotasInteractionListener mListener;
+    
+    public MyNotaRecyclerViewAdapter(List<Nota> notaList, NotasInteractionListener mListener) {
+        this.mValues = notaList;
+        this.mListener = mListener;
+    }
+
+
+ 
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_item, parent, false);
+        return new ViewHolder(view);
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        //holder.mIdView.setText(mValues.get(position).id);
+        //holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public Nota mItem;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    }
